cba7aa149b349b23af9131d4c12e983f540786e3
Zeydi <zeidiwassa@gmail.com>
2023-07-30 09:58:08 +0200

    added home content and fixed styling for Header and BaseLayout

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,24 +1,60 @@
+import React, { useState } from "react";
 import Link from "next/link";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+} from "reactstrap";
+
+const BsNavLink = (props) => {
+  const { href, title } = props;
+  return (
+    <Link href={href} legacyBehavior>
+      <a className="nav-link port-navbar-link">{title}</a>
+    </Link>
+  );
+};
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
-    <>
-      <Link href="/" legacyBehavior>
-        <a>Home</a>
-      </Link>
-      <Link href="about/" legacyBehavior>
-        <a>About</a>
-      </Link>
-      <Link href="/portfolios" legacyBehavior>
-        <a>Portfolios</a>
-      </Link>
-      <Link href="/blogs" legacyBehavior>
-        <a>Blogs</a>
-      </Link>
-      <Link href="/cv" legacyBehavior>
-        <a>Cv</a>
-      </Link>
-    </>
+    <div>
+      <Navbar
+        className="port-navbar port-default absolute"
+        color="transparent"
+        dark
+        expand="md"
+      >
+        <Link href="/" legacyBehavior>
+          <a className="port-navbar-brand">Oussama ZEYDI</a>
+        </Link>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/" title="Home" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/about" title="About" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/portfolios" title="Portfolios" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/blogs" title="Blogs" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/cv" title="Cv" />
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 };
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/main.scss";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
