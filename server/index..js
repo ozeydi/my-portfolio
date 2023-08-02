cfd702896617f4ce5b99fa8d6c7b82dd510f0cd8
Zeydi <zeidiwassa@gmail.com>
2023-08-02 10:33:01 +0200

    removed unusual files

--- server/index..js
+++ server/index..js
@@ -1,8 +1,9 @@
 import express from "express";
 
 const app = express();
-
 const PORT = process.env.PORT || 3001;
+
+app.get("/test", (req, res) => res.json({ message: "working" }));
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
